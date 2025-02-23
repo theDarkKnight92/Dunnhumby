af2a619730ffad892e5e545c550033d3fe9833f9
theDarkKnight92 <54811340+theDarkKnight92@users.noreply.github.com>
2025-02-23 20:30:21 +0530

    Add files via upload

---
+++ AdCampaignService/src/main/java/com/dunnhumby/AdCampaignService/AdCampaignServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dunnhumby.AdCampaignService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdCampaignServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdCampaignServiceApplication.class, args);
+	}
+
+}
---
+++ AdCampaignService/src/test/java/com/dunnhumby/AdCampaignService/AdCampaignServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunnhumby.AdCampaignService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdCampaignServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
